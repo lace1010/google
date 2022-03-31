027e342ee3cdc62a62f73448032e0c23f9834843
Hunter Lacefield <lace1010@gmail.com>
2022-03-31 16:00:55 -0400

    cleaned up util folder

--- util/useGeoLocation.js
+++ util/useGeoLocation.js
@@ -1,5 +1,4 @@
-import { STATES } from "mongoose";
-import { React, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const useGeoLocation = () => {
   const [location, setLocation] = useState({
